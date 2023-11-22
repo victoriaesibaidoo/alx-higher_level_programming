8c266d0123cde56f5413b53399394f0abcda4c20
victoriaesibaidoo <victoriaesibaidoo@gmail.com>
2023-11-22 00:45:04 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
+"""A module for square"""
 
 
 class Square:
     """Represent a square."""
-
-    def __init__(self, size):
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new square.
-        """
-        self.__size = size
+    pass
