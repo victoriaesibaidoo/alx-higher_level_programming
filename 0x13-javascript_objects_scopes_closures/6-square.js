1eb6f2b715f0a978d347c20d6e88408a94f1e647
victoriaesibaidoo <victoriaesibaidoo@gmail.com>
2024-03-01 18:52:26 -0800

    updated

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
-const SquareP = require('./5-square');
 
-class Square extends SquareP {
+const supSquare = require('./5-square');
+
+class Square extends supSquare {
   charPrint (c) {
-    if (c === undefined) {
+    if (c == null) {
       c = 'X';
     }
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
-      }
-      console.log(s);
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
